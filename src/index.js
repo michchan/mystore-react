55a378076448f1ea69a861b4e99debee344fab35
Michael Chan <me@michchan.com>
2018-03-31 17:58:14 +0800

    Config .env and add API url

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Init dotenv
+require('dotenv').config();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
