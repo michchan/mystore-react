370fed1f6887b2a5eb6d40983e9e76d2af3dd0d2
Michael Chan <me@michchan.com>
2018-03-31 19:12:06 +0800

    add app error boundary component

---
+++ src/components/state/AppErrorBoundary/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './style.css';
+
+export class AppErrorBoundary extends Component {
+    static defaultProps = {}
+    static propTypes = {}
+
+    state = { 
+        hasError: false,
+        error: null,
+        info: null,
+    };
+
+    componentDidCatch(error, info) {
+        // Display fallback UI
+        this.setState({ hasError: true, error, info });
+        // You can also log the error to an error reporting service
+        console.log(error, info);
+    }
+    
+    render() {
+        if (!this.state.hasError) {
+            // You can render any custom fallback UI
+            return (
+                <div className={'container'}>
+                    <p className={'title'}>{
+                        'Opps! An error occured.'
+                    }</p>
+                    <p className={'error-title'}>{ `Error: ${this.state.error}` }</p>
+                    <p className={'error-info'}>{this.state.info}</p>
+                </div>
+            );
+        }
+        
+        return this.props.children;
+    }
+};
+
+export default AppErrorBoundary;
