6b544883f073592606925113758726bb9c858ab0
Michael Chan <me@michchan.com>
2018-04-03 03:02:21 +0800

    Refractor codes for using only one root file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+import { AppErrorBoundary } from './components/state/AppErrorBoundary';
+import {
+    HomeScene,
+} from './scenes';
+import { IS_DEV, disableLogging } from './lib';
+
+if (!IS_DEV) {
+    console.disableYellowBox = true;
+    disableLogging();
+}
 
 // Init dotenv
 require('dotenv').config();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <AppErrorBoundary>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/' component={HomeScene}/>
+                </Switch>
+            </BrowserRouter>
+        </Provider>
+    </AppErrorBoundary>
+), document.getElementById('root'));
 registerServiceWorker();
