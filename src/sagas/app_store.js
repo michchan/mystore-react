27cb7b88d0866d33fc16481ecd34efa9d2303b12
Michael Chan <me@michchan.com>
2018-04-03 20:01:31 +0800

    Add init fetch and fetching top grossing apps saga,

--- src/sagas/app_store.js
+++ src/sagas/app_store.js
@@ -5,15 +5,50 @@ import {
     FETCHING_TOP_FREE_APPS_STARTED, 
     FETCHING_TOP_FREE_APPS_ERROR, 
     FETCHING_TOP_FREE_APPS_SUCCESS,
-    START_FETCHING_TOP_FREE_APPS_FLOW
+    START_FETCHING_TOP_FREE_APPS_FLOW,
+    FETCHING_TOP_GROSSING_APPS_STARTED,
+    FETCHING_TOP_GROSSING_APPS_SUCCESS,
+    FETCHING_TOP_GROSSING_APPS_ERROR,
+    INIT_FETCH_STARTED,
+    INIT_FETCH_SUCCESS,
+    INIT_FETCH_ERROR
 } from '../actions';
 
-import { getTopFreeAppsAPI, topFreeAppsParams } from '../api';
-import { getAPICaller } from '../lib/api';
+import { getTopFreeAppsAPI, topFreeAppsParams, appStoreEntryMap, getTopGrossingAppsAPI, topGrossingAppsParams, appStoreMetaMap } from '../api';
+import { getAPICaller, mapApiFields } from '../lib/api';
 import { handleApiCall, handleApiCallConfigKeys as configs } from '../lib/saga';
+import { normalize } from 'normalizr';
+import { appStoreEntriesNormalizedSchema } from '../normalizers/app_store';
 
 /**
- * Some Saga
+ * Fetch all on app initilized
+ */
+export const initFetchFlow = function * initFetchFlow(action) {
+    yield call(handleApiCall, {
+        [configs.startedType]: INIT_FETCH_STARTED,
+        [configs.successType]: INIT_FETCH_SUCCESS,
+        [configs.errorType]: INIT_FETCH_ERROR,
+        [configs.callFunction]: {
+            topFreeApps: getAPICaller(getTopFreeAppsAPI),
+            topGrossingApps: getAPICaller(getTopGrossingAppsAPI),
+        },
+        [configs.callParams]: {
+            topFreeApps: [{ [topFreeAppsParams.LIMIT]: 10 }],
+            topGrossingApps: [{ [topGrossingAppsParams.LIMIT]: 10 }],
+        }, 
+        [configs.processData]: (data) => {
+            return {
+                topFreeApps: processAppStoreData(data.topFreeApps),
+                topGrossingApps: processAppStoreData(data.topGrossingApps),
+            };
+        },
+        [configs.bindProcessedDataToAction]: true,
+        [configs.requestTimeout]: 5000,
+    });
+}
+
+/**
+ * Fetch Top Free Apps Saga
  */
 export const fetchingTopFreeAppsFlow = function* fetchingTopFreeAppsFlow(action) {
     const { limit } = action;
@@ -26,6 +61,41 @@ export const fetchingTopFreeAppsFlow = function* fetchingTopFreeAppsFlow(action)
         [configs.callParams]: [{ 
             [topFreeAppsParams.LIMIT]: limit,
         }],
+        [configs.processData]: processAppStoreData,
+        [configs.bindProcessedDataToAction]: true,
         [configs.requestTimeout]: 5000,
     });
+}
+
+/**
+ * Fetch Top Grossing Apps Saga
+ */
+export const fetchingTopGrossingAppsFlow = function* fetchingTopGrossingAppsFlow(action) {
+    const { limit } = action;
+
+    yield call(handleApiCall, {
+        [configs.startedType]: FETCHING_TOP_GROSSING_APPS_STARTED,
+        [configs.successType]: FETCHING_TOP_GROSSING_APPS_SUCCESS,
+        [configs.errorType]: FETCHING_TOP_GROSSING_APPS_ERROR,
+        [configs.callFunction]: getAPICaller(getTopGrossingAppsAPI),
+        [configs.callParams]: [{ 
+            [topGrossingAppsParams.LIMIT]: limit,
+        }],
+        [configs.processData]: processAppStoreData,
+        [configs.bindProcessedDataToAction]: true,
+        [configs.requestTimeout]: 5000,
+    });
+}
+
+const processAppStoreData = (data) => {
+    const { entry, ...meta } = data.feed;
+    const parsedEntries = mapApiFields(entry, appStoreEntryMap);
+    const parsedMeta = mapApiFields(meta, appStoreMetaMap);
+    const normalizedData = normalize(parsedEntries, appStoreEntriesNormalizedSchema);
+
+    return {
+        meta: parsedMeta,
+        ...normalizedData,
+        entities: normalizedData.entities.apps,
+    }
 }
--- src/sagas/root.js
+++ src/sagas/root.js
@@ -1,10 +1,20 @@
 import { fork, takeEvery, takeLatest, all } from 'redux-saga/effects';
-import { fetchingTopFreeAppsFlow } from './app_store';
-import { START_FETCHING_TOP_FREE_APPS_FLOW } from '../actions';
+import { 
+    fetchingTopFreeAppsFlow, 
+    fetchingTopGrossingAppsFlow, 
+    initFetchFlow
+} from './app_store';
+import { 
+    START_FETCHING_TOP_FREE_APPS_FLOW,
+    START_FETCHING_TOP_GROSSING_APPS_FLOW, 
+    START_INIT_FETCH_FLOW
+} from '../actions';
 
 export const rootSaga = function * root() {
     yield all([
+        takeLatest(START_INIT_FETCH_FLOW, initFetchFlow),
         takeLatest(START_FETCHING_TOP_FREE_APPS_FLOW, fetchingTopFreeAppsFlow),
+        takeLatest(START_FETCHING_TOP_GROSSING_APPS_FLOW, fetchingTopGrossingAppsFlow),
     ]);
 }
 
